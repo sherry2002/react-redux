1ae75112382e4a03c30ed45a23c478bd73e51480
sherry2002 <muhammadshehryar93@gmail.com>
2022-05-22 20:55:57 +0500

    Redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Login from './services/login';
 import Home from './services/Home';
-import Register from './services/Product';
+import Register from './services/Register';
 import { useSelector } from 'react-redux';
 import {
   BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
+import Error from './services/Error';
 
 function App() {
   const { userData } = useSelector(
@@ -24,7 +25,9 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home /> } />
+          <Route path="/" element={userData ? <Home /> : <Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<Error />} />
         </Routes>
       </BrowserRouter>
 
---
+++ src/services/Error.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Error() {
+  return (
+    <div>Error 404 <br/> page not found</div>
+  )
+}
+
+export default Error
